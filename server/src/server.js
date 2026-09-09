63ff0706c95724dccdb59eb23eaafc8da1b2e78f
Nguyễn Long <phamthuy3001976@gmail.com>
2026-09-10 06:28:00 +0700

    Update server configuration

--- server/src/server.js
+++ server/src/server.js
@@ -91,7 +91,7 @@ gameSocket(io);
 // START SERVER
 // =====================================================
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 server.listen(
     PORT,
