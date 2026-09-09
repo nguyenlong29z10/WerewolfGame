f4fcd184a1d841bc6aaa9941afe0a2ec3449ab9e
Nguyễn Long <phamthuy3001976@gmail.com>
2026-09-10 06:55:46 +0700

    Configure production CORS

--- server/src/server.js
+++ server/src/server.js
@@ -21,8 +21,9 @@ const app = express();
 
 const allowedOrigins = [
     "http://localhost:5173",
-    "http://localhost:5174"
-];
+    "http://localhost:5174",
+    process.env.CLIENT_URL
+].filter(Boolean);
 
 
 app.use(
