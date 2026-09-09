ee7099fe3700b1bd4cd693a7ed3fd99338f1dcd6
Nguyễn Long <phamthuy3001976@gmail.com>
2026-09-10 06:44:37 +0700

    Connect frontend to environment API

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,7 +1,10 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
-    autoConnect: true
-});
+const socket = io(
+    import.meta.env.VITE_API_URL,
+    {
+        autoConnect: true
+    }
+);
 
 export default socket;
