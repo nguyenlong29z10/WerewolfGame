512dbe2bd0557b2e0a817f78c60b43079ddf9f7b
Nguyễn Long <phamthuy3001976@gmail.com>
2026-09-10 07:54:31 +0700

    fix cors for new vercel domain

--- server/src/server.js
+++ server/src/server.js
@@ -22,6 +22,7 @@ const app = express();
 const allowedOrigins = [
     "http://localhost:5173",
     "http://localhost:5174",
+    "https://nguyenlong-mshn.vercel.app",
     process.env.CLIENT_URL
 ].filter(Boolean);
 
